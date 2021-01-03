da0d41ec20b88661a410dfc5ebd9a30a17d0c821
IgroRik <48213289+IgroRik@users.noreply.github.com>
2021-01-03 16:12:30 +0200

    Add alive field

--- NPC/models.py
+++ NPC/models.py
@@ -4,12 +4,13 @@ from DjangoForDnD import settings
 
 
 class Person(models.Model):
-    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     text = models.TextField()
     role = models.CharField(max_length=50, default="")
     map = models.CharField(max_length=50, default="")
     created_date = models.DateTimeField(default=timezone.now)
+    alive = models.BooleanField(default="True")
 
     def __str__(self):
         return self.name
