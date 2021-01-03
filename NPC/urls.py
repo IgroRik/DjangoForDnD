2ef91ab9093e4e3bdee22e6fa630bd8f7fb7b093
IgroRik <48213289+IgroRik@users.noreply.github.com>
2021-01-04 00:53:50 +0200

    Add icon field Add details page Add add page(form)

--- NPC/urls.py
+++ NPC/urls.py
@@ -1,6 +1,18 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from DjangoForDnD import settings
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('details/<int:npcid>/', views.get_npc_id, name='get_npc_id'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- NPC/views.py
+++ NPC/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.utils import timezone
+
+from django.shortcuts import render, redirect
 from .models import Person
+from .forms import PersonForm
 
 
 def post_list(request):
     posts = Person.objects.all().order_by("name")
     return render(request, 'npc/post_list.html', {'posts': posts})
+
+
+def get_npc_id(request, npcid):
+    npc = Person.objects.get(id=npcid)
+    return render(request, 'npc/details.html', {'npc': npc})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('get_npc_id', npcid=post.pk)
+    else:
+        form = PersonForm
+    return render(request, 'npc/post_edit.html', {'form': form})
